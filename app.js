f63591694fcf51b7f6b109b2f4537189497bcc6e
jeongmingi <mo4811@naver.com>
2020-10-22 13:52:09 +0900

    middleware 구성 및 라우터 구성

---
+++ app.js
@@ -0,0 +1,27 @@
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import { userRouter } from "./routers/userRouter.js";
+import { videoRouter } from "./routers/videoRouter.js";
+import { globalRouter } from "./routers/globalRouter.js";
+
+const app = express();
+
+const handleHome = (req, res) => res.send("Hello from home");
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev")); // 어떠한 방식과 어디 폴더에 접속하는지 알수있음 + 컬러추가
+
+// app.use(morgan("tiny")); // 어떠한 방식과 어디 폴더에 접속하는지 알수있음
+// app.use(morgan("combined")); // 어떤 종류의 접속인지 어떤 브라우저인지 등등에 대하여 알 수 있다.
+
+app.use("/", globalRouter);
+app.use("/user", userRouter); // use의 의미는 /user에 경로에 접속하면 이 router 전체를 사용하겠다는 의미
+app.use("/video", videoRouter);
+
+export default app;
